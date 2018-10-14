0bc188553028b73cd8326c8a5c8f508351ef386f
Keith Bauer <onesadcookie@gmail.com>
2018-10-15 00:02:06 +1300

    use JSON to define client->server and server->client message formats
    (we should generate both rust & elm code from a common IDL in future!)

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,25 +1,73 @@
 import Html exposing (Html, button, br, input, div, text)
 import Html.Events exposing (onInput, onClick)
+import Json.Decode exposing (Decoder, int, string, oneOf)
+import Json.Decode.Pipeline exposing (decode, required)
+import Json.Encode
 import WebSocket
+
+type ServerCommand =
+    Echo { message : String }
+  | Timestamp { when : Int }
+
+type ClientCommand =
+    Shout { message : String }
 
 type alias Model =
   { input : String
   , output : List String
   }
 type Msg =
-    Append(String)
+    FromServer(ServerCommand)
+  | ServerError
   | Type(String)
   | Send
+
+serverCommandFieldDecoder : String -> Decoder ServerCommand
+serverCommandFieldDecoder t =
+  case t of
+    "Echo" ->
+      decode (\message -> Echo { message = message })
+        |> required "message" string
+    "Timestamp" ->
+      decode (\when -> Timestamp { when = when })
+        |> required "when" int
+    _ ->
+      Json.Decode.fail "Unexpected type"
+
+serverCommandDecoder : Decoder ServerCommand
+serverCommandDecoder = 
+  Json.Decode.field "type" string
+    |> Json.Decode.andThen serverCommandFieldDecoder
+
+encodeClientCommand : ClientCommand -> Json.Encode.Value
+encodeClientCommand command =
+  case command of
+    Shout shout ->
+      Json.Encode.object [
+        ("type", Json.Encode.string "Shout"),
+        ("message", Json.Encode.string shout.message)
+      ]
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Append(s) ->
-      ({ model | output = model.output ++ [s] }, Cmd.none)
+    FromServer(serverCommand) ->
+      case serverCommand of
+          Echo echo ->
+            ({ model | output = model.output ++ [echo.message] }, Cmd.none)
+          Timestamp timestamp ->
+            ({ model | output = model.output ++ ["--" ++ (toString timestamp.when) ]}, Cmd.none)
+    ServerError ->
+       -- could consider displaying it on the client?
+      (model, Cmd.none)
     Type(s) ->
       ({ model | input = s }, Cmd.none)
     Send ->
-      ({ model | input = "" }, WebSocket.send "ws://localhost:3030/echo" model.input)
+      let clientCommand = Shout { message = model.input }
+          json = encodeClientCommand clientCommand
+          message = Json.Encode.encode 0 json
+      in
+        ({ model | input = "" }, WebSocket.send "ws://localhost:3030/echo" message)
 
 view : Model -> Html Msg
 view model =
@@ -33,9 +81,15 @@ view model =
       ]
     ]
 
+parseWSMessage : String -> Msg
+parseWSMessage s =
+  Json.Decode.decodeString serverCommandDecoder s
+    |> Result.map FromServer
+    |> Result.withDefault ServerError
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    WebSocket.listen "ws://localhost:3030/echo" Append
+    WebSocket.listen "ws://localhost:3030/echo" parseWSMessage
 
 main : Program Never Model Msg
 main =
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,57 @@
 extern crate dotenv;
 extern crate pretty_env_logger;
+#[macro_use] extern crate serde_derive;
+extern crate serde_json;
 extern crate warp;
 
 use warp::{Filter, Future, Stream};
+use warp::filters::ws::Message;
+
+#[derive(Deserialize)]
+#[serde(tag = "type")]
+enum ClientCommand {
+    Shout { message: String },
+}
+
+#[derive(Serialize)]
+#[serde(tag = "type")]
+enum ServerCommand {
+    Echo { message: String },
+    Timestamp { when: i64 },
+}
+
+struct EchoError;
+
+// TODO: use error-chain
+impl std::convert::From<()> for EchoError {
+
+    fn from(_: ()) -> EchoError {
+        EchoError
+    }
+
+}
+
+impl std::convert::From<serde_json::Error> for EchoError {
+
+    fn from(_: serde_json::Error) -> EchoError {
+        EchoError
+    }
+
+}
+
+fn echo_err(message: Message) -> Result<Message, EchoError> {
+    let message_text = message.to_str()?;
+    let command: ClientCommand = serde_json::from_str(message_text)?;
+    let response = match command {
+        ClientCommand::Shout { message } => ServerCommand::Echo { message: message },
+    };
+    let response_text = serde_json::to_string(&response)?;
+    Ok(Message::text(response_text))
+}
+
+fn echo(message: Message) -> Message {
+    echo_err(message).unwrap_or(Message::text(serde_json::to_string(&ServerCommand::Timestamp { when: 0 }).unwrap()))
+}
 
 fn main() {
     dotenv::dotenv().ok();
@@ -17,7 +66,9 @@ fn main() {
             ws.on_upgrade(|websocket| {
                 // Just echo all messages back...
                 let (tx, rx) = websocket.split();
-                rx.forward(tx)
+                rx
+                    .map(echo)
+                    .forward(tx)
                     .map(|_| ())
                     .map_err(|e| {
                         eprintln!("websocket error: {:?}", e);
